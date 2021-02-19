7dec2b3269f9e5039f22c5de6e714a06ddb958a6
Mikle <mikle0706@icloud.com>
2021-02-19 18:40:56 +0300

    изменить линтер

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import logo from "assets/image/logo.svg";
-import style from "container/App.module.scss";
+import React from 'react';
+import logo from 'assets/image/logo.svg';
+import style from 'container/App.module.scss';
 
 function App() {
   return (
     <div className={style.App}>
-      <header className={style["App-header"]}>
-        <img src={logo} className={style["App-logo"]} alt="logo" />
+      <header className={style['App-header']}>
+        <img src={logo} className={style['App-logo']} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className={style["App-link"]}
+          className={style['App-link']}
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./container/App";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './container/App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 // If you want to start measuring performance in your app, pass a function
